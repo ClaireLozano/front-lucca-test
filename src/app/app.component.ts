7836204afa27ac882944e96cd6d5699d9fdb2f0c
Claire Lozano <claire.lozano@live.fr>
2023-11-12 20:51:09 +0100

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Component } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { NxWelcomeComponent } from './nx-welcome.component'
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'front-lucca-test-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NxWelcomeComponent, RouterModule],
+    selector: 'front-lucca-test-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front-lucca-test';
+    title = 'front-lucca-test'
 }
